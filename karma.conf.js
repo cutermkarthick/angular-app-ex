c877df9b920a8b27d55f0c531527d9f339cf56b8
Karthick <cute.rmkarthick@gmail.com>
2021-05-25 12:52:54 +0530

    added ng test coverage

--- karma.conf.js
+++ karma.conf.js
@@ -1,10 +1,13 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-const process = require('process');
-process.env.CHROME_BIN = require('puppeteer').executablePath();
+
 
 module.exports = function (config) {
+
+  const process = require('process');
+  process.env.CHROME_BIN = require('puppeteer').executablePath();
+
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
